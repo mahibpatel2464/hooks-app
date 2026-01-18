bb7e2c4dcee04271bd084804ae7e14151782ed90
mahibpatel2464 <mahibpatel2464@gmail.com>
2026-01-18 15:12:28 +0530

    Create App component with Counter and FocusInput

---
+++ app.jsx
@@ -0,0 +1,14 @@
+import Counter from "./components/Counter";
+import FocusInput from "./components/FocusInput";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <Counter />
+      <FocusInput />
+    </div>
+  );
+}
+
+export default App;
